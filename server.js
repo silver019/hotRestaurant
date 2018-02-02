4a690e6f13607e2987b2ed054b38ab4bc8f73a01
ADM MacKay <adminmama@marineharvest.net>
2018-02-01 19:39:14 -0500

    server changes

--- server.js
+++ server.js
@@ -10,22 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/home', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index2.html'));
+  res.sendFile(path.join(__dirname+'/tables.html'));
 
 });
-app.get('/tables', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index3.html'));
-
-});
-
-app.get('/reserved', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index4.html'));
+app.get('/reserve', function (req, res) {
+  res.sendFile(path.join(__dirname+'/reserve.html'));
 
 });
 
 
-app.get('/test', (req, res) => {
-  res.send('Hello test');
-});
- 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
